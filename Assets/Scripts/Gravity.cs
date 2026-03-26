9c46c498d35ac4bd8ad0eb5d2462277956d5f5b6
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-26 10:27:35 +0700

    Gravity

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Gravity : MonoBehaviour
+{
+
+    public static List<Gravity> otherObj;
+    private Rigidbody rb;
+    const float G = 0.00667f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if ( otherObj == null)
+        {
+            otherObj = new List<Gravity>();
+        }
+        otherObj.Add(this);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (Gravity obj in otherObj)
+        {
+            if (obj != this)
+            {
+               Attract(obj);
+            }
+            
+        }
+    }
+
+    void Attract( Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+
+        float distance = direction.magnitude;
+
+        if (distance == 0) return;
+
+        float forceMagnitude = G * (rb.mass * otherRb.mass) / Mathf.Pow(distance, 2);
+        Vector3 gravitationForce = forceMagnitude * direction.normalized;
+        otherRb.AddForce(gravitationForce);
+    }
+}
